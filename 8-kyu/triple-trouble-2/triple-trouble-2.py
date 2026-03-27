dcccc4e9a8011332d6ad60bb82ea402e1ea281e7
Muhammad Sarumi <61973852+muhsar27@users.noreply.github.com>
2026-03-27 12:21:59 +0100

    Add solution - CodeHub

---
+++ 8-kyu/triple-trouble-2/triple-trouble-2.py
@@ -0,0 +1,12 @@
+def triple_trouble(one, two, three):
+    new_str = ""
+    for i in range(len(one)):
+    
+        new_str += one[i]
+        new_str += two[i]
+        new_str += three[i]
+        
+                
+    
+    
+    return new_str
