3ddb0fb0e5d33e36c0934ad82e669b2a70738a6d
Muhammad Sarumi <61973852+muhsar27@users.noreply.github.com>
2026-05-22 11:42:46 +0100

    Add solution - CodeHub

---
+++ 5-kyu/moving-zeros-to-the-end/moving-zeros-to-the-end.go
@@ -0,0 +1,17 @@
+package kata
+​
+func MoveZeros(arr []int) []int {
+  //var zeros int 
+  for i := 0 ; i < len(arr)-1 ; i++ {
+    for j := 0 ; j < len(arr)-1; j++ {
+      if arr[j] == 0 && arr[j+1] != 0 {
+        arr[j],arr[j+1] = arr[j+1],arr[j]
+      }
+    }
+  }
+  return arr     
+}
+​
+​
+​
+​
